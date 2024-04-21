994766150fab7a247068475406187188621c3890
whr888z <3327407524@qq.com>
2024-04-21 13:08:15 +0800

    home页的整体架构拆分和分类的渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div style="height: 880px">我是home页</div>
+  <div class="container">
+    <homeCategory></homeCategory>
+    <homeBanner></homeBanner>
+  </div>
+
+  <homehot></homehot>
+  <homenew></homenew>
+  <homeproduct></homeproduct>
 </template>
+
+<script setup>
+import homeBanner from './components/homeBanner.vue'
+import homeCategory from './components/homeCategory.vue'
+import homehot from './components/homeHot.vue'
+import homenew from './components/homeNew.vue'
+import homeproduct from './components/homeProduct.vue'
+</script>
+
+<style lang="scss" scoped></style>
