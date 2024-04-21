f746d4665e0aaebdb39bb5f8ebc6f4473b9a97c5
whr888z <3327407524@qq.com>
2024-04-21 13:39:05 +0800

    banner轮播图

--- src/views/Home/components/homeBanner.vue
+++ src/views/Home/components/homeBanner.vue
@@ -1,5 +1,37 @@
-<script setup></script>
+<script setup>
+import { homeBannerAPI } from '@/api/home'
+import { ref } from 'vue'
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await homeBannerAPI()
+  console.log(res)
+  bannerList.value = res.data.result
+}
+getBanner()
+</script>
 
 <template>
-  <div>Homebanner</div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home-banner {
+  width: 990px;
+  height: 500px;
+  position: absolute;
+  left: 250px;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
